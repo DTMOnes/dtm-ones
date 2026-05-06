7caa7c6f9f745ba8f2f6c2e84c3b2609ba4e2fc6
FrancoLedesma <francoledesma100@gmail.com>
2026-05-06 19:35:17 -0300

    Changes

--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+// Next
+import Link from "next/link";
+
 // Styles
 import styles from "./styles.module.scss";
 
 export default function HeroSection() {
   return (
     <section className={styles.container}>
-      <h1 className={styles.title}>
-        The name <br /> talent <br /> trusts
-      </h1>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          The name <br /> talent <br /> trusts
+        </h1>
+
+        <Link href="/roster" className={styles.button}>
+          Check Our Roster
+        </Link>
+      </div>
+
       <video
         className={styles.video}
         src="https://gpkuudzvcgjdfquuodpo.supabase.co/storage/v1/object/public/public-assets/YTDown.com_Shorts_Steph-Curry-s-shot-in-Slow-Motion-shorts_Media_iQVNOyQES0A_001_1080p.mp4"
